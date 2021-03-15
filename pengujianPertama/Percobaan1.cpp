1b4f7c3b86c7fde7b9522b991c303c4189f1dc38
lyrihkaesa <kaesalyrih@gmail.com>
2021-03-15 18:59:36 +0700

    menambahkan Percobaan1

---
+++ pengujianPertama/Percobaan1.cpp
@@ -0,0 +1,25 @@
+/* program pertukaran */
+/* mempertukarkan nilai A dan B, nilai A dan B dibaca terlebih dahulu */
+
+#include<stdio.h>
+#include<conio.h>
+#include <stdlib.h>
+
+main(){
+/* DEKLARASI */
+int A,B,C;
+/* ALGORITMA*/
+system("cls");
+printf("A = ");
+scanf("%d",&A);
+printf("B = ");
+scanf("%d",&B);
+/* proses pertukaran */
+C = A;/* simpan nilai A ke variabel penampung C */
+A = B;/* Isi nilai variabel A dengan nilai variabel B */
+B = C;/* Isi nilai variabel B dengan nilai variabel C (C berisi Nilai semula A *
+/* tulis nilai setelah pertukaran */
+printf("A = %d \n",A);
+printf("B = %d \n",B);
+system("pause");
+}
