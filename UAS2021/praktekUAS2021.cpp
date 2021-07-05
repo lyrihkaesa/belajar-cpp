c65e992a34a32554a9c737f6ec7548da801f96d9
lyrihkaesa <kaesalyrih@gmail.com>
2021-07-05 18:33:42 +0700

    UAS2021

--- UAS2021/praktekUAS2021.cpp
+++ UAS2021/praktekUAS2021.cpp
@@ -5,10 +5,11 @@
 #define almtEntity "Jl. Merdeka Raya 17"
 #define kotaEntity "Semarang"
 #define maxHuruf 16
+#define maxHurufNIP 5
 
 // struct data pelanggan
 struct sPgw {
-    char nip[maxHuruf], namaPegawai[maxHuruf], jenisKelamin[maxHuruf], status[maxHuruf];
+    char nip[maxHurufNIP], namaPegawai[maxHuruf], jenisKelamin[maxHuruf], status[maxHuruf];
     int jumlahAnak, gajiPokok, tunjIstri, tunjAnak, gajiKotor, pajak, biayaJabatan, gajiBersih;
 };
 sPgw dataPegawai[100];
@@ -17,6 +18,7 @@ int a = 0;
 
 // Fungsi Prototype (Function Prototype)
 void garisH(int kolom, int baris, int kolom_awal=2);
+void garisHT(int kolom, int baris, int kolom_awal=2);
 void garisV(int kolom, int baris, int baris_awal=2);
 void entityProp(int kolom, int baris);
 void templateUtama();
@@ -44,23 +46,24 @@ void prosesGajiBersih(int kolom, int baris);
 void laporanGaji(int kolom, int baris);
 void judulLaporan(int kolom, int baris);
 void cetakData(int kolom, int baris);
+void bubble(int kolom, int baris);
 
 // Deklarasi Garis Utama
-int gh = 83, gv = 20;
+int gh = 88, gv = 20;
 
 void main() {
     int pilih;
     do {
         clrscr();
         templateUtama();
-        menuUtama(20, 8);
-        pilihanMenu(15, (gv-1)); cin >> pilih;
+        menuUtama(0, 8);
+        pilihanMenu(0, (gv-1)); cin >> pilih;
         switch (pilih) {
             case 1 : inputData(5, 8);
             break;
-            case 2 : cout << "unknow";
+            case 2 : bubble(0, gv-4);
             break;
-            case 3 : laporanGaji(5, 8);
+            case 3 : laporanGaji(3, 8);
             break;
         }
     } while (pilih != 4);
@@ -72,6 +75,13 @@ void garisH(int kolom, int baris, int kolom_awal){
         gotoxy(i, baris); cout << "_";
     }
 }
+
+void garisHT(int kolom, int baris, int kolom_awal){
+    for(int i=kolom_awal;i<kolom;i++){
+        gotoxy(i, baris); cout << "-";
+    }
+}
+
 void garisV(int kolom, int baris, int baris_awal){
     for(int i=baris_awal;i<baris;i++){
         gotoxy(kolom, i); cout << "|";
@@ -100,24 +110,28 @@ void templateUtama(){
 
 // Tampilan Menu Utama
 void menuUtama(int kolom, int baris){
+    int jT = 30;
+    kolom = ((gh-jT)/2)+kolom;
     gotoxy(kolom, baris++); cout << "Menu Utama";
     baris++;
-    gotoxy(kolom, baris++); cout << "1. Input Data";
-    gotoxy(kolom, baris++); cout << "2. Urutkan Data";
+    gotoxy(kolom, baris++); cout << "1. Input Gaji Pegawai";
+    gotoxy(kolom, baris++); cout << "2. Urutkan Data (Bubble Sort)";
     gotoxy(kolom, baris++); cout << "3. Laporan Gaji Pegawai";
     gotoxy(kolom, baris++); cout << "4. Exit (Keluar)";
     garisH(gh, (gv-3));
 }
 
 void pilihanMenu(int kolom, int baris){
-    gotoxy(kolom, baris); cout << "Pilihan anda [1,2,3,4=exit] : ";    
+    char tulisan[] = "Pilihan anda [1,2,3,4=exit] : ";
+    int jT = jumlahHuruf(tulisan);
+    gotoxy((((gh-jT)/2)+kolom), baris); cout << tulisan;    
 }
 
 void tampilanEdit(int kolom, int baris){
     gotoxy(kolom, baris++); cout << "1. NIP               = ";
     gotoxy(kolom, baris++); cout << "2. Nama Pegawai      = ";
-    gotoxy(kolom, baris++); cout << "3. Jenis Kelamin     = ";
-    gotoxy(kolom, baris++); cout << "4. Status            = ";
+    gotoxy(kolom, baris++); cout << "3. Jenis Kelamin[P/W]= ";
+    gotoxy(kolom, baris++); cout << "4. Status [0,1,2,3,4]= ";
     gotoxy(kolom, baris++); cout << "   Jumlah anak       = ";
     gotoxy(kolom, baris++); cout << "5. Gaji Pokok        = ";
 }
@@ -136,8 +150,10 @@ void inputData(int kolom, int baris){
     do {
         clrscr();
         int barisF = baris;
+        char judulAtas[] = "Input Gaji Pegawai";
+        int xJA = jumlahHuruf(judulAtas);
         templateUtama();
-        gotoxy((kolom+20), barisF++); cout << "Input Gaji Pegawai";
+        gotoxy(((gh-xJA)/2), barisF++); cout << judulAtas;
         barisF = barisF+1;
         tampilanEdit(kolom, barisF);
         int kolomA = kolom + maxHuruf + 1 + 25; // 2 adalah spasi antar kolom, 25 panjang huruf tertinggi di tampilanEdit() yaitu (24+1);
@@ -153,7 +169,7 @@ void inputData(int kolom, int baris){
 
 // Input Gaji Pegawai
 void ketikInputData(int kolom, int baris){
-    isiNIP(kolom, baris++, maxHuruf);
+    isiNIP(kolom, baris++, maxHurufNIP);
     isiNamaPegawai(kolom, baris++, maxHuruf);
     isiJenisKelamin(kolom, baris++);
     isiStatus(kolom, baris++);
@@ -178,13 +194,15 @@ void outputData(int kolom, int baris){
 
 void laporanGaji(int kolom, int baris){
     clrscr();
+    char judulAtas[] = "Laporan Gaji Pegawai";
+    int xJA = jumlahHuruf(judulAtas);
     templateUtama();
-    gotoxy((kolom+20), baris++); cout << "Laporan Gaji Pegawai";
+    gotoxy(((gh-xJA)/2), baris++); cout << judulAtas;
     baris++;
-    garisH(gh, 10);
-    garisH(gh, 12);
-    judulLaporan(3, 11);
-    cetakData(3, 13);
+    garisHT(gh, baris++);
+    judulLaporan(kolom, baris++);
+    garisHT(gh, baris++);
+    cetakData(kolom, baris++);
     getch();
 }
 
@@ -192,7 +210,11 @@ void judulLaporan(int kolom, int baris){
     gotoxy(kolom, baris); cout << "No.";
     kolom += 5;
     gotoxy(kolom, baris); cout << "NIP ";
-    kolom += 6;
+    kolom += maxHurufNIP;
+    gotoxy(kolom, baris); cout << " ";
+    kolom += 1;
+    gotoxy(kolom, baris); cout << "Nama Peg.";
+    kolom += maxHuruf + 1;
     gotoxy(kolom, baris); cout << "Gaji Pokok";
     kolom += 12;
     gotoxy(kolom, baris); cout << "Tunj. Istri";
@@ -207,19 +229,24 @@ void judulLaporan(int kolom, int baris){
 void cetakData(int kolom, int baris){
     int len = a;
     for (int i=0; i < len; i++) {
-    gotoxy(kolom, baris); cout << (i+1);
-    kolom += 5;
-    gotoxy(kolom, baris); cout << dataPegawai[i].nip;
-    kolom += 6;
-    gotoxy(kolom, baris); cout << dataPegawai[i].gajiPokok;
-    kolom += 12;
-    gotoxy(kolom, baris); cout << dataPegawai[i].tunjIstri;
-    kolom += 13;
-    gotoxy(kolom, baris); cout << dataPegawai[i].tunjAnak;
-    kolom += 12;
-    gotoxy(kolom, baris); cout << dataPegawai[i].gajiKotor;
-    kolom += 10;
-    gotoxy(kolom, baris); cout << dataPegawai[i].gajiBersih;
+    int kolomi = kolom;
+    gotoxy(kolomi, baris); cout << (i+1);
+    kolomi += 5;
+    gotoxy(kolomi, baris); cout << dataPegawai[i].nip;
+    kolomi += maxHurufNIP;
+    gotoxy(kolomi, baris); cout << " ";
+    kolomi += 1;
+    gotoxy(kolomi, baris); cout << dataPegawai[i].namaPegawai;
+    kolomi += maxHuruf + 1;
+    gotoxy(kolomi, baris); cout << dataPegawai[i].gajiPokok;
+    kolomi += 12;
+    gotoxy(kolomi, baris); cout << dataPegawai[i].tunjIstri;
+    kolomi += 13;
+    gotoxy(kolomi, baris); cout << dataPegawai[i].tunjAnak;
+    kolomi += 12;
+    gotoxy(kolomi, baris); cout << dataPegawai[i].gajiKotor;
+    kolomi += 10;
+    gotoxy(kolomi, baris); cout << dataPegawai[i].gajiBersih;
     baris++;
     }
 }
@@ -248,8 +275,9 @@ void jumlahSpasi(int kolom, int baris, int mxHuruf){
 
 void isiNIP(int kolom, int baris, int mxHuruf){
     jumlahTitik(kolom, baris, mxHuruf);
+    jumlahSpasi((kolom+5), baris, 11);
     gotoxy(kolom, baris); gets(dataPegawai[a].nip);
-    if(jumlahHuruf(dataPegawai[a].nip) < 5) {
+    if(jumlahHuruf(dataPegawai[a].nip) != mxHuruf) {
         isiNIP(kolom, baris, mxHuruf);
     }
 }
@@ -352,4 +380,24 @@ void prosesBiayaJabatan(int kolom, int baris){
 void prosesGajiBersih(int kolom, int baris){
     dataPegawai[a].gajiBersih = dataPegawai[a].gajiKotor - dataPegawai[a].pajak + dataPegawai[a].biayaJabatan;
     gotoxy(kolom, baris); cout << dataPegawai[a].gajiBersih;
+}
+
+void bubble(int kolom, int baris) {
+    char info[] = "Data sudah diurutkan! (Buble Sort)";
+    int jH = jumlahHuruf(info);
+    sPgw tempdata;
+    for(int i=1;i<a;i++) {
+        for(int j=0;j<a-i;j++) {
+            // lakukkan penukaran data.........
+            if(strcmp(dataPegawai[j].nip, dataPegawai[j+1].nip) > 0) {
+                tempdata = dataPegawai[j];
+                dataPegawai[j] = dataPegawai[j+1];
+                dataPegawai[j+1] = tempdata;
+            }
+        }
+    }
+    
+    // mencetak keterangan (sudah diurutkan).........
+    gotoxy((((gh-jH)/2)+kolom), baris); cout << info;
+    getch();
 }
